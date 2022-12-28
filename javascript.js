1df5f82765dafcc41d645b5d87d6b9e608383084
Caio Abud <82781059+caioabudd@users.noreply.github.com>
2022-12-27 23:13:58 -0300

    Pokemon API
    Puxa dados de uma API

---
+++ javascript.js
@@ -0,0 +1,34 @@
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+    return  `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+               
+            </ol>
+            <img src="https://www.pngmart.com/files/11/Pokemon-Bulbasaur-PNG-Pic.png" alt="" height="">
+        </div>
+        
+    </li>
+
+`
+
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+pokeApi.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+})
